ce20ca364b4d469554998d95aaf271556dd1550f
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-09 15:11:21 +0200

    добавил файлы проэкта Xcode

---
+++ toDoList("Tasks")/toDoList("Tasks")/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDoList("Tasks")
+//
+//  Created by Sergey Golenko on 09.12.2020.
+//  Copyright © 2020 Sergey Golenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
