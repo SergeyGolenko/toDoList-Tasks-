bc4a3ad0dccf05afb88e3cfd55b0552b58c145cd
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-10 16:50:20 +0200

    Работа с таблицей DataSource

--- toDoList("Tasks")/toDoList("Tasks")/TaskTableViewController.swift
+++ toDoList("Tasks")/toDoList("Tasks")/TaskTableViewController.swift
@@ -9,24 +9,36 @@
 import UIKit
 
 class TaskTableViewController: UITableViewController {
+    
+    let color = CGColor(srgbRed: 38, green: 166, blue: 255, alpha: 1)
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.backgroundColor = #colorLiteral(red: 0.1615069807, green: 0.7159221768, blue: 1, alpha: 1)
+        
     }
-
     // MARK: - Table view data source
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return.lightContent
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 50    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "100$"
+        return cell
     }
 
   
 
 }
+
